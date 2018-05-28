91b37ec03a562775a262d7649aabf06baad38cb1
Dylan Simmer <goldenoblivion@gmail.com>
2018-05-28 22:00:31 +1000

    integrated stuff

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,7 +11,9 @@ import (
 	"path/filepath"
 	"time"
 
+	"cloud.google.com/go/datastore"
 	"github.com/gorilla/mux"
+	"google.golang.org/api/iterator"
 
 	"github.com/rs/cors"
 	"gopkg.in/mgo.v2"
@@ -71,6 +75,49 @@ func Init() {
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
+	}
+
+	ctx := context.Background()
+
+	// Set your Google Cloud Platform project ID.
+	projectID := "firm-champion-204312"
+
+	// Creates a client.
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the kind for the new entity.
+	kind := "Post"
+	// Sets the name/ID for the new entity.
+	name := time.Now().String()
+	// Creates a Key instance.
+	postKey := datastore.NameKey(kind, name, nil)
+
+	// Creates a Post instance.
+	post := Post{
+		Content: "<div></div>",
+	}
+
+	// Saves the new entity.
+	if _, err := client.Put(ctx, postKey, &post); err != nil {
+		log.Fatalf("Failed to save task: %v", err)
+	}
+
+	query := datastore.NewQuery("Task")
+
+	it := client.Run(ctx, query)
+	for {
+		var post Post
+		_, err := it.Next(&post)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error fetching next task: %v", err)
+		}
+		fmt.Printf("Post %q, \n", post.Content)
 	}
 	exPath := filepath.Dir(ex)
 
