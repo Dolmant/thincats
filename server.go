5a9d27e91572f35d1863de204aafe54afba2d270
Dylan Simmer <goldenoblivion@gmail.com>
2018-06-08 16:16:18 +1000

    fixed datastore, apis should work now

--- server.go
+++ server.go
@@ -16,8 +16,13 @@ import (
 	"google.golang.org/api/iterator"
 
 	"github.com/rs/cors"
-	"gopkg.in/mgo.v2"
 )
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 // Post holds the dynamic content
 type Post struct {
@@ -31,59 +36,85 @@ func (f HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	f(w, r)
 }
 
-func postsHandler(w http.ResponseWriter, r *http.Request) {
-	session := getSession()
-	postsCollection := session.DB("thincats").C("posts")
-	if r.Method == http.MethodGet {
-		getQuery := mgo.Query{}
-		results := make(map[string]string)
-		err := postsCollection.Find(getQuery).All(results)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("error"))
-		} else {
-			encoded, err := json.Marshal(results)
+func postsHandler(ctx context.Context, client *datastore.Client) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		if r.Method == http.MethodGet {
+			postsCollection := []Post{}
+			query := datastore.NewQuery("Post")
+
+			it := client.Run(ctx, query)
+			for {
+				var post Post
+				_, err = it.Next(&post)
+				if err == iterator.Done {
+					break
+				}
+				if err != nil {
+					fmt.Println("Error fetching next post")
+					break
+				}
+				fmt.Printf("Post %q, \n", post.Content)
+				postsCollection = append(postsCollection, post)
+			}
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				w.Write([]byte("error"))
 			} else {
-				w.Write(encoded)
+				encoded, err := json.Marshal(postsCollection)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("error"))
+				} else {
+					w.Write(encoded)
+				}
+			}
+		} else if r.Method == http.MethodPost {
+			// Sets the kind for the new entity.
+			kind := "Post"
+			// Sets the name/ID for the new entity.
+			name := time.Now().String()
+			// Creates a Key instance.
+			postKey := datastore.NameKey(kind, name, nil)
+
+			// Creates a Post instance.
+			var post Post
+			// Saves the new entity.
+			if _, err := client.Put(ctx, postKey, &post); err != nil {
+				log.Fatalf("Failed to save task: %v", err)
+			}
+
+			content, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("error"))
+			}
+			err = json.Unmarshal(content, &post)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("error"))
+			} else {
+				w.Write([]byte("success"))
+			}
+			_, err = client.Put(ctx, &datastore.Key{}, post)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("error"))
+			} else {
+				w.Write([]byte("success"))
 			}
 		}
-	} else if r.Method == http.MethodPost {
-		content, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("error"))
-		}
-		err = postsCollection.Insert(content)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("error"))
-		} else {
-			w.Write([]byte("success"))
-		}
 	}
-}
-
-func getSession() *mgo.Session {
-	dbAddress := "127.0.0.1:8080"
-	if os.Getenv("db") != "" {
-		dbAddress = os.Getenv("db")
-	}
-	session, err := mgo.Dial(dbAddress)
-	if err != nil {
-		panic(err)
-	}
-	return session
 }
 
 // Init constructs the server and routes. With a bigger project this might be separated into different files and accept config options
 func Init() {
 	log.Printf("Server logging started at: %s", time.Now())
 	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
+	check(err)
+	exPath := filepath.Dir(ex)
+	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", exPath+string(os.PathSeparator)+"thincats-c1f1e998f5d9.json")
 	}
 
 	ctx := context.Background()
@@ -100,42 +131,9 @@ func Init() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	// Sets the kind for the new entity.
-	kind := "Post"
-	// Sets the name/ID for the new entity.
-	name := time.Now().String()
-	// Creates a Key instance.
-	postKey := datastore.NameKey(kind, name, nil)
-
-	// Creates a Post instance.
-	post := Post{
-		Content: "<div></div>",
-	}
-
-	// Saves the new entity.
-	if _, err := client.Put(ctx, postKey, &post); err != nil {
-		log.Fatalf("Failed to save task: %v", err)
-	}
-
-	query := datastore.NewQuery("Task")
-
-	it := client.Run(ctx, query)
-	for {
-		var post Post
-		_, err := it.Next(&post)
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Error fetching next task: %v", err)
-		}
-		fmt.Printf("Post %q, \n", post.Content)
-	}
-	exPath := filepath.Dir(ex)
-
 	mux := mux.NewRouter()
 
-	mux.HandleFunc("/posts/", postsHandler)
+	mux.HandleFunc("/posts/", postsHandler(ctx, client))
 
 	fs := http.FileServer(http.Dir(exPath + string(os.PathSeparator) + "SPA" + string(os.PathSeparator) + "dist"))
 
